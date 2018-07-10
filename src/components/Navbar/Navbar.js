7ef26b7c2897ddb061b95ad4b1e99b076201beda
Dalamo <dalamo90@gmail.com>
2018-07-09 23:41:49 -0500

    styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,19 +5,19 @@ import "./Navbar.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => (
   <nav className="navbar">
-    <ul>
-      <li>
+ 
+      <div>
         {/* <Link className="navbar-brand" to="/"> */}
-          memoryGame
+          Cat Memory
         {/* </Link> */}
-      </li>
-      <li>
+      </div>
+      <div>
         Click an image to begin!
-      </li>
-      <li>
-        Score: {props.score}| Top Score: {props.topScore}
-      </li>
-    </ul>
+      </div>
+      
+        Score: {props.score} || Top Score: {props.topScore}
+      
+    
   </nav>
 );
 
